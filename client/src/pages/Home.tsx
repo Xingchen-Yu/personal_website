15e378c535bed068f2ff4ba8cb90d0a3b17a8447
xvy5021 <36270727-xvy5021@users.noreply.replit.com>
2025-01-26 22:09:18 +0000

    Agent query: Can you check if the homepage looks properly organized after removing the Research Interests section?
    Remove ResearchInterests component from Home page.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/96626f2d-d1ea-45ae-8a79-e7ba0e266587/7bbb1536-f06f-41f9-a874-5f927d639131.jpg

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/Hero";
-import ResearchInterests from "@/components/ResearchInterests";
 import TechnicalSkills from "@/components/TechnicalSkills";
 import Contact from "@/components/Contact";
 
@@ -7,7 +6,6 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <ResearchInterests />
       <TechnicalSkills />
       <Contact />
     </div>
